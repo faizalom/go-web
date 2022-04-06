c6f10c7fafd3f7d277bf3b3dac5b86b427b5afb5
Faizal Ahamed <faizalom@hotmail.com>
2022-04-06 19:49:06 +0530

    WIP

--- runner/coindcx/main.go
+++ runner/coindcx/main.go
@@ -34,8 +34,27 @@ func main() {
 					defer wg.Done()
 					candleMean := marketcontroller.CandleMean{}
 					marketcontroller.GetCandles(pair, &candleMean)
-					// if candleMean.Max < 5 {
-					// 	fmt.Printf("%#v\n", candleMean.Max)
+					if candleMean.Max < 8 {
+						return
+					}
+
+					fmt.Println(candleMean.VariencePer)
+
+					//tickerCalc := model.GetTickerCalc(t)
+					//fmt.Println(tickerCalc.NowHighPer)
+					// lastPrice24, err := strconv.ParseFloat(t.LastPrice, 32)
+					// if err != nil {
+					// 	log.Println(err)
+					// }
+					//fmt.Println(candleMean.Candles10[0].Low - lastPrice24)
+					//fmt.Println(t.LastPrice)
+
+					// low24, err := strconv.ParseFloat(t.Low, 32)
+					// if err != nil {
+					// 	log.Println(err)
+					// }
+					// if candleMean.Candles10[0].Low == low24 {
+					// 	fmt.Println(low24)
 					// }
 					tickerCalc := model.GetTickerCalc(t)
 					fmt.Println(tickerCalc.LowHighPer)
