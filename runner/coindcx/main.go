cab0ca9c7183f24110e4a968654d865333230a7f
Faizal Ahamed <faizalom@hotmail.com>
2022-04-07 19:55:03 +0530

    LastPrice5

--- runner/coindcx/main.go
+++ runner/coindcx/main.go
@@ -5,16 +5,84 @@ import (
 	"helper/api/coindcx"
 	"helper/model"
 	"log"
+	"strconv"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/faizalom/go-web/controllers/marketcontroller"
 )
 
 var BaseCoin string
 var wg sync.WaitGroup
+var LastPrice5 map[string][]float64
+
+func SetLastPrice5() {
+	LastPrice5 = make(map[string][]float64)
+	ticker, err := coindcx.GetExchange()
+	if err != nil {
+		log.Println(err)
+	}
+	for _, t := range ticker {
+		if !strings.Contains(t.Market, BaseCoin) {
+			continue
+		}
+		LastPrice5[t.Market] = make([]float64, 5)
+	}
+}
+
+func getRank(xc []float64) int {
+	a := xc[4]
+	b := xc[3]
+	c := xc[2]
+	d := xc[1]
+	e := xc[0]
+
+	rank := 0
+	if a >= b {
+		rank += 1
+	}
+	if b >= c {
+		rank += 1
+	}
+	if c >= d {
+		rank += 1
+	}
+	if d >= e {
+		rank += 1
+	}
+	if a > e {
+		rank += 1
+	}
+	return rank
+}
 
 func main() {
+	BaseCoin = "USDT"
+	SetLastPrice5()
+
+	for range time.Tick(time.Second * 30) {
+
+		ticker, err := coindcx.GetExchange()
+		if err != nil {
+			log.Println(err)
+		}
+		for _, t := range ticker {
+			if !strings.Contains(t.Market, BaseCoin) {
+				continue
+			}
+			lastPrice, _ := strconv.ParseFloat(t.LastPrice, 64)
+			LastPrice5[t.Market] = append(LastPrice5[t.Market][1:], lastPrice)
+			rank := getRank(LastPrice5[t.Market])
+			if rank == 5 {
+				fmt.Println(t.Market, LastPrice5[t.Market][0])
+			}
+		}
+		fmt.Println("-----------------------------")
+	}
+}
+
+func main3() {
 	s := make([]int, 5)
 	fmt.Println(s[1:])
 
