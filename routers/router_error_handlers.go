f1d0e798438e07d3358cc6cefa02b0d543340fc4
faizalom <faizalom@hotmail.com>
2024-12-20 15:43:39 +0530

    Update router_error_handlers.go

--- routers/router_error_handlers.go
+++ routers/router_error_handlers.go
@@ -1,34 +1,32 @@
 package routers
 
 import (
-	"html/template"
 	"net/http"
 
 	"github.com/faizalom/go-web/config"
+	"github.com/faizalom/go-web/lib"
 )
-
-var Template = template.Must(template.ParseGlob(config.ThemePath))
 
 type H map[string]any
 
 // This function is run user enters invalid url
 func fileNotFoundHandler(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
-	Template.ParseFiles(config.ThemePath + "/views/error.html")
-	Template.ExecuteTemplate(w, "error.html", H{"title": "404 | Not Found"})
+	lib.Template.ParseFiles(config.Path.Theme + "/views/error.html")
+	lib.Template.ExecuteTemplate(w, "error.html", H{"title": "404 | Not Found"})
 }
 
 // This function is run user sends a wrong method
 func methodNotAllowedHandler(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
 
-	Template.ParseFiles(config.ThemePath + "/views/error.html")
-	Template.ExecuteTemplate(w, "error.html", H{"title": "405 | Method Not Allowed"})
+	lib.Template.ParseFiles(config.Path.Theme + "/views/error.html")
+	lib.Template.ExecuteTemplate(w, "error.html", H{"title": "405 | Method Not Allowed"})
 }
 
 // This function is run during the CSRF token expired
 func pageExpiredHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(419)
-	Template.ParseFiles(config.ThemePath + "/views/error.html")
-	Template.ExecuteTemplate(w, "error.html", H{"title": "419 | Page Expired"})
+	lib.Template.ParseFiles(config.Path.Theme + "/views/error.html")
+	lib.Template.ExecuteTemplate(w, "error.html", H{"title": "419 | Page Expired"})
 }
