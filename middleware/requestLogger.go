f59febbe28b137fbb979b06f82dcdb595eab7b2f
faizalom <faizalom@hotmail.com>
2024-12-20 15:40:13 +0530

    Update requestLogger.go

--- middleware/requestLogger.go
+++ middleware/requestLogger.go
@@ -13,7 +13,7 @@ func RequestLogger(targetMux http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer func(w http.ResponseWriter, r *http.Request) {
 			if rec := recover(); rec != nil {
-				log.Println(r.URL.Path, rec)
+				log.Println("Panic: ", r.URL.Path, rec)
 				http.Error(w, rec.(error).Error(), http.StatusInternalServerError)
 			}
 		}(w, r)
@@ -26,7 +26,7 @@ func RequestLogger(targetMux http.Handler) http.Handler {
 func AccessLog(r *http.Request) {
 	l := log.New(log.Writer(), log.Prefix(), log.Flags())
 
-	logFile, err := os.OpenFile(config.AccessLogFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	logFile, err := os.OpenFile(config.LogFile.AccessLog, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Println(err)
 	}
